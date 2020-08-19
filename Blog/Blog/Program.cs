06bea988aa0ffc48b1a02f2b71139017743bd466
Michael Vassilyev <rebusmv@gmail.com>
2020-08-19 18:00:38 +0600

    Добавлен usrMgr

--- Blog/Blog/Program.cs
+++ Blog/Blog/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +16,34 @@ namespace Blog
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            var scope = host.Services.CreateScope();
+
+            var ctx = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            var usrMgr = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            var roleMgr = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            ctx.Database.EnsureCreated();
+
+            var adminRole = new IdentityRole("Admin");
+
+            if (!ctx.Roles.Any())
+            {
+                roleMgr.CreateAsync(adminRole).GetAwaiter().GetResult();
+            }
+
+            if (!ctx.Users.Any(u=>u.UserName == "admin"))
+            {
+                var adminUser = new IdentityUser
+                {
+                    UserName = "admin",
+                    Email = "admin@test.com"
+                };
+                usrMgr.CreateAsync(adminUser,"password");
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
